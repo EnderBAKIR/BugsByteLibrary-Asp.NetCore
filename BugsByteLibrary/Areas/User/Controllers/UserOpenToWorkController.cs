7e4ef7a1a3ee56a37430061a157be39e34067e5b
Ender Bakır <enderyx_123@hotmail.com>
2024-02-25 14:15:23 +0300

    UserOpenToWorkController/Index / Index / UpdateOpenToWork
    UserOpenToWorkController userın iş başvurları ile ilgili işlemleri yapması için oluşturuldu ve düzenlendi , usera ait iş başvuruları listeleniyor ve user bunları güncelleyebiliyor. Gerekli validasyonlar yapıldı.

---
+++ BugsByteLibrary/Areas/User/Controllers/UserOpenToWorkController.cs
@@ -0,0 +1,55 @@
+﻿using Core.Layer.IService;
+using Core.Layer.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BugsByteLibrary.Areas.User.Controllers
+{
+    [Area("User")]
+    [Authorize]
+    public class UserOpenToWorkController : Controller
+    {
+
+        private readonly IOpenToWorkService _openToWorkService;
+
+        private readonly UserManager<AppUser> _userManager;
+
+        public UserOpenToWorkController(IOpenToWorkService openToWorkService, UserManager<AppUser> userManager)
+        {
+            _openToWorkService = openToWorkService;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            var openToWorks = await _openToWorkService.GetOpenWorksByUserIdAsync(user.Id);
+
+
+            return View(openToWorks);
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateOpenToWork(string id)
+        {
+            var openToWork = await _openToWorkService.GetOpenToWorkByIdAsnc(id);
+
+            return View(openToWork);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateOpenToWork(OpenToWork openToWork)
+        {   
+            openToWork.Status = true;
+            openToWork.UpdateTime= DateTime.Now;
+
+            await _openToWorkService.UpdateOpenToWorkAsync(openToWork);
+
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
