089d0086593e1ae066aa2ccec0a9d40081b3021b
Ender Bakır <enderyx_123@hotmail.com>
2024-02-25 11:38:59 +0300

    HiringsController
    Sitenin içinde iş başvurularını listelemek için HiringsController yapıldı.

---
+++ BugsByteLibrary/Controllers/HiringsController.cs
@@ -0,0 +1,33 @@
+﻿using Core.Layer.IService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BugsByteLibrary.Controllers
+{
+    public class HiringsController : Controller
+    {
+        private readonly IHiringService _hiringService;
+
+        public HiringsController(IHiringService hiringService)
+        {
+            _hiringService = hiringService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+            var hirings = await _hiringService.GetAllHiringAsync();
+
+
+            return View(hirings);
+        }
+
+
+        public async Task<IActionResult> GetHiringDetail(string id)
+        {
+            var hiring = await _hiringService.GetHiringByIdAsync(id);
+
+            return View(hiring);
+
+        }
+    }
+}
---
+++ Repository.Layer/Repositories/OpenToWorkRepository.cs
@@ -0,0 +1,49 @@
+﻿using Core.Layer.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Layer.Repositories
+{
+    public class OpenToWorkRepository
+    {
+        private readonly AppDbContext _appDbContext;
+        private readonly DbSet<OpenToWork> _openToWorkSet;
+
+        public OpenToWorkRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+            _openToWorkSet =  _appDbContext.Set<OpenToWork>();
+        }
+
+
+
+        public async Task<OpenToWork> AddOpenToWorkAsync(OpenToWork openToWork)
+        {
+            await _openToWorkSet.AddAsync(openToWork);
+
+            return openToWork;
+
+        }
+        public async Task<IEnumerable<OpenToWork>> GetOpenWorksByUserIdAsync(int id)
+        {
+            var openToWork = await _openToWorkSet.Include(x => x.AppUser).Include(x=>x.Hiring).Where(x => x.AppUserId == id).ToListAsync();
+
+            return openToWork;
+        }
+
+        public void UpdateOpenToWorkAsync(OpenToWork openToWork)
+        {
+            _openToWorkSet.Update(openToWork);
+        }
+
+        public void DeleteOpenToWorkAsync(OpenToWork openToWork)
+        {
+            _openToWorkSet.Remove(openToWork);
+        }
+        
+    }
+}
