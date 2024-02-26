5a80ce2af196966a7838b29c2a4484d3a0fc094c
Ender Bakır <enderyx_123@hotmail.com>
2024-02-26 14:07:40 +0300

    Admin Layout Düzenlemeleri

---
+++ BugsByteLibrary/Areas/Admin/Controllers/AdminDefaultController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BugsByteLibrary.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminDefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
