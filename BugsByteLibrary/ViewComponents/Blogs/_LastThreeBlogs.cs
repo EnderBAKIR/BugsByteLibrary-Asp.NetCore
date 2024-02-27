1f0de112afd019c86f50c06ecc1e8aac030b9ea6
Ender Bakır <enderyx_123@hotmail.com>
2024-02-27 17:36:02 +0300

    Tasarımsal Düzenlemeler Ve Ufak Hataların Giderilmesi

---
+++ BugsByteLibrary/ViewComponents/Blogs/_LastThreeBlogs.cs
@@ -0,0 +1,24 @@
+﻿using Core.Layer.IService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BugsByteLibrary.ViewComponents.Blogs
+{
+    public class _LastThreeBlogs : ViewComponent
+    {
+        private readonly IBlogService _blogService;
+
+        public _LastThreeBlogs(IBlogService blogService)
+        {
+            _blogService = blogService;
+        }
+
+        [HttpGet]
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            var blogs = await _blogService.GetLastSixBlogAsync(id);
+
+
+            return View(blogs);
+        }
+    }
+}
